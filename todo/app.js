bc538afdf93dc302176a38edf6dd3df99399c53e
dipanshunamdev799 <111988188+dipanshunamdev799@users.noreply.github.com>
2024-06-26 16:52:20 +0530

    todo-app

---
+++ todo/app.js
@@ -0,0 +1,36 @@
+let descInput = document.getElementsByClassName("desc-input")[0];
+let taskInput = document.getElementsByClassName("task-input")[0];
+let submitButton = document.getElementsByClassName("submit-button")[0];
+let list = document.getElementsByClassName("list")[0];
+
+submitButton.addEventListener('click',()=>{
+    let task = String(taskInput.value);
+    let desc = descInput.value;
+    if(task=='' || desc==''){
+        alert("Give some input");
+    }else{
+
+        taskInput.value = "";
+        descInput.value = "";
+
+        let element = document.createElement('div');
+        element.classList.add("task-element");
+
+        let para = document.createElement('p');
+        let fullTaskDescription = task.toUpperCase() + " : " + desc;
+        para.append(fullTaskDescription);
+
+        element.append(para);
+
+        let button = document.createElement('button');
+        button.onclick = ()=>{
+            button.parentElement.remove();
+        }
+
+        button.classList.add('del');
+        button.innerText = "DELETE";
+        
+        element.append(button);
+        list.append(element);
+    }
+});
